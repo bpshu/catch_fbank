ba4460842b637441467d27c1166f38d24ee5ac6e
j-shubeiping-jk <shubeiping-jk@360shuke.com>
2022-06-09 12:58:01 +0800

    .

--- test/run.sh
+++ test/run.sh
@@ -1,24 +1,24 @@
-wav_scp="/mnt/d/gitlab/VAD/test/data/testData_byHand/wav.scp"
+#!/usr/bin
 wav_scp="/mnt/d/github/catch_fbank/test/tmp/wav.scp"
-featfile="/mnt/d/github/catch_fbank/test/tmp/feat.2"
-
-# wav_scp="tmp/wav.scp"
 featfile="/mnt/d/github/catch_fbank/test/tmp/feat.1"
 rm -rf $featfile*
-
 featscp="/mnt/d/github/catch_fbank/test/tmp/feat.scp"
+
+num_samples_to_run=4
+num_threads=2
+
 ../build/catch_fbank_multithread_main_2nd \
     --wav_scp ${wav_scp} \
     --featfile ${featfile} \
     --featscp ${featscp} \
-    --num_samples_to_run 4 \
-    --num_threads 2
+    --num_samples_to_run $num_samples_to_run \
+    --num_threads $num_threads
 
 cat ${featscp}.* | sort >${featscp}
 rm -f ${featscp}.*
 
-for x in $featfile.*; do
-    # echo $x
-    ../../kaldi-trunk/src/featbin/copy-feats --binary=true ark,t:$x ark:$x.ark
-done
-#
+# for x in $featfile.*; do
+#     # echo $x
+#     ../../kaldi-trunk/src/featbin/copy-feats --binary=true ark,t:$x ark:$x.ark
+# done
+# #
